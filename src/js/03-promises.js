e75675293119105e01f88c02031b87cf797e6244
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-04-17 16:34:42 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,19 +8,18 @@ const refs = {
   amount: document.querySelector('[name="amount"]'),
 }
 
-
 refs.form.addEventListener('click', onPromiseCreate);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-  if (shouldResolve) {
-    resolve({ position, delay });
-    } else {
-    reject({ position, delay });
-   }
-  }, delay);
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
 
@@ -36,8 +35,8 @@ function onPromiseCreate(e) {
 
   createPromise(i, promiseDelay)
     .then(({ position, delay }) => {
-       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
     .catch(({ position, delay }) => {
       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
